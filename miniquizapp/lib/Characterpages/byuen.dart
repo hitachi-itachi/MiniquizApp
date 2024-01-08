7ad28f5350294fb6091a937c279a2ac623c91e69
Alastair Ng <yusenng88@gmail.com>
2024-01-09 01:12:41 +0800

    Chore: Done up individual character pages

--- miniquizapp/lib/Characterpages/byuen.dart
+++ miniquizapp/lib/Characterpages/byuen.dart
@@ -2,14 +2,14 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class Sypnosis extends StatelessWidget {
-  const Sypnosis({super.key});
+class byuen extends StatelessWidget {
+  const byuen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Sypnosis",
+          title: Text("Byuen",
               style: TextStyle(
                 fontSize: 30.0,
                 fontWeight: FontWeight.bold,
@@ -28,14 +28,15 @@ class Sypnosis extends StatelessWidget {
             )),
           ),
           Positioned(
+              top: 10,
               child: Container(
-            height: 200,
-            width: 410,
-            child: Image.asset(
-              "images/sypnosis.jpeg",
-              fit: BoxFit.fill,
-            ),
-          )),
+                height: 200,
+                width: 410,
+                child: Image.asset(
+                  "images/byuen.jpg",
+                  fit: BoxFit.fitHeight,
+                ),
+              )),
           Positioned(
               top: 250,
               left: 10,
@@ -44,7 +45,7 @@ class Sypnosis extends StatelessWidget {
                 height: 500,
                 width: double.infinity,
                 child: Text(
-                    "Yoon Jong-woo (Im Shi-wan) a young guy in his 20s moves from the countryside to Seoul after getting a job offer.\n\n Not being from a rich household,he looks for a cheap place to rent and stumbles upon Eden Studio, a hostel that shares its kitchen and bathroom with other problematic and abnormal residents.\n\n He is not happy about his residence but decides to put up with it for 6 months until he saves enough money to move out. But when disturbing events start taking place in the hostel, he soon realizes that he unknowingly got trapped in a nightmare.",
+                    "Yoon Jong-woo (Im Shi-wan) a young guy in his 20s moves from the countryside to Seoul after getting a job offer.\n\nNot being from a rich household,he looks for a cheap place to rent and stumbles upon Eden Studio, a hostel that shares its kitchen and bathroom with other problematic and abnormal residents.\n\n He is not happy about his residence but decides to put up with it for 6 months until he saves enough money to move out. But when disturbing events start taking place in the hostel, he soon realizes that he unknowingly got trapped in a nightmare.",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
--- miniquizapp/lib/Pages/Character.dart
+++ miniquizapp/lib/Pages/Character.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:miniquizapp/Characterpages/Dentist.dart';
+import 'package:miniquizapp/Characterpages/Jong.dart';
+import 'package:miniquizapp/Characterpages/Um.dart';
+import 'package:miniquizapp/Characterpages/byuen.dart';
+import 'package:miniquizapp/Characterpages/hong.dart';
 
 class character extends StatelessWidget {
   const character({Key? key}) : super(key: key);
@@ -37,14 +42,19 @@ class character extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Container(
-                    width: double.infinity,
-                    height: 150,
-                    color: Colors.red,
-                    child: Image.asset(
-                      "images/um.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+                      width: double.infinity,
+                      height: 150,
+                      color: Colors.red,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Um()));
+                        },
+                        child: Image.asset(
+                          "images/um.png",
+                          fit: BoxFit.cover,
+                        ),
+                      )),
                   Padding(
                     padding: EdgeInsets.only(top: 10),
                     child: Text(
@@ -74,10 +84,17 @@ class character extends StatelessWidget {
                     width: double.infinity,
                     height: 150,
                     color: Colors.red,
-                    child: Image.asset(
-                      "images/dentist.jpg",
-                      fit: BoxFit.cover,
-                    ),
+                    child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Dentist()));
+                        },
+                        child: Image.asset(
+                          "images/dentist.jpg",
+                          fit: BoxFit.cover,
+                        )),
                   ),
                   Padding(
                     padding: EdgeInsets.only(top: 10),
@@ -107,22 +124,26 @@ class character extends StatelessWidget {
                   width: double.infinity,
                   height: 150,
                   color: Colors.red,
-                  child: Image.asset(
-                    "images/dentist.jpg",
-                    fit: BoxFit.cover,
-                  ),
+                  child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => hong()));
+                      },
+                      child: Image.asset(
+                        "images/Hong.jpg",
+                        fit: BoxFit.cover,
+                      )),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    "The Dentist",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Hong Nam Bok",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
               ]),
             ),
           ),
@@ -140,15 +161,21 @@ class character extends StatelessWidget {
                   width: double.infinity,
                   height: 150,
                   color: Colors.red,
-                  child: Image.asset(
-                    "images/dentist.jpg",
-                    fit: BoxFit.cover,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Jong()));
+                    },
+                    child: Image.asset(
+                      "images/jong.jpeg",
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 10),
                   child: Text(
-                    "The Dentist",
+                    "Jong U",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
@@ -161,7 +188,7 @@ class character extends StatelessWidget {
           ),
           Positioned(
             top: 495,
-            left: 10,
+            left: MediaQuery.of(context).size.width * 0.30,
             child: Container(
               width: MediaQuery.of(context).size.width * 0.45,
               height: 200,
@@ -169,18 +196,23 @@ class character extends StatelessWidget {
               child:
                   Column(mainAxisAlignment: MainAxisAlignment.start, children: [
                 Container(
-                  width: double.infinity,
-                  height: 150,
-                  color: Colors.red,
-                  child: Image.asset(
-                    "images/dentist.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.red,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => byuen()));
+                      },
+                      child: Image.asset(
+                        "images/byuen.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    )),
                 Padding(
                   padding: EdgeInsets.only(top: 10),
                   child: Text(
-                    "The Dentist",
+                    "Byuen",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
@@ -191,39 +223,6 @@ class character extends StatelessWidget {
               ]),
             ),
           ),
-          Positioned(
-            top: 495,
-            left: MediaQuery.of(context).size.width * 0.55,
-            right: 10,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.45,
-              height: 200,
-              color: Colors.orange,
-              child:
-                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                Container(
-                  width: double.infinity,
-                  height: 150,
-                  color: Colors.red,
-                  child: Image.asset(
-                    "images/dentist.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    "The Dentist",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ]),
-            ),
-          )
         ],
       ),
     );
